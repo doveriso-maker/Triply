404fbee005a5e6db50b98c1b5016fa107508e921
doveriso-maker <doveriso@gmail.com>
2026-09-05 19:44:51 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='triply-core-v1-13';
-const CORE=['/','/site.html','/index.html','/questionnaire.html','/trip-data.json','/events.json','/manifest.webmanifest','/icon-192.png','/icon-512.png','/triply-logo-transparent.png'];
+const CACHE='triply-core-v1-14';
+const CORE=['/','/site.html','/trip-app.html','/questionnaire.html','/trip-data.json','/events.json','/manifest.webmanifest','/icon-192.png','/icon-512.png','/triply-logo-transparent.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
